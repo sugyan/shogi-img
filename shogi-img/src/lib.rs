8a0e6891666af8b30c9a829643dff05e50e43022
Yoshihiro Sugi <sugi1982@gmail.com>
2024-07-26 14:11:52 +0900

    feat: Add HighlightSquare to generator (#1)
    * Add HighlightSquare to generator
    
    * Fix docs

--- shogi-img/src/lib.rs
+++ shogi-img/src/lib.rs
@@ -3,7 +3,7 @@
 //! There are several selectable styles for the board and pieces used in image generation.
 mod generator;
 
-pub use crate::generator::Generator;
+pub use crate::generator::{AsPosition, Generator};
 pub use image;
 pub use shogi_core;
 
@@ -27,7 +27,18 @@ pub enum PiecesStyle {
     HitomojiGothic,
 }
 
+/// Hightlight mode for last moved pieces.
+#[derive(Default)]
+pub enum HighlightSquare {
+    #[default]
+    None,
+    LastMoveTo,
+}
+
 /// A simple function to generate an image from a position using the default styles.
-pub fn pos2img(position: &shogi_core::PartialPosition) -> image::RgbaImage {
+pub fn pos2img<T>(position: &T) -> image::RgbaImage
+where
+    T: AsPosition,
+{
     Generator::default().generate(position)
 }
